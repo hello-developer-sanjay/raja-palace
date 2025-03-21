89ee0702f31f5e353e7a69f0af8170690fa241a0
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-03-21 13:59:55 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
   swcMinify: true,
   output: 'standalone',
   trailingSlash: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://rajpalaceandconventionorg.vercel.app' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://raja-palace-eight.vercel.app' : '',
 };
 
 module.exports = nextConfig; 
